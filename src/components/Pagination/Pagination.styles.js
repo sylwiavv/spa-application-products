9e94fd1c8f3c69754572fb20b8a42f38c0d84f27
Sylwia Mentel <sylwiavv@gmail.com>
2022-05-28 21:56:28 +0200

    Add pagination component

---
+++ src/components/Pagination/Pagination.styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const PaginationWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  max-width: 400px;
+  margin: 16px auto;
+`;
--- src/views/Root.js
+++ src/views/Root.js
@@ -4,14 +4,30 @@ import { GlobalStyle } from 'assets/styles/GlobalStyle';
 import { theme } from 'assets/styles/theme';
 import { api, endpoints, params } from 'api';
 import { ProductsWrapper, Product } from '../components/ProductWrapper/ProductWrapper.styles';
+import axios from 'axios';
+import { PaginationWrapper } from '../components/Pagination/Pagination.styles';
 
 const Root = () => {
   const [dataAPI, setData] = useState([]);
 
+  // useEffect(() => {
+  //   // .get(endpoints.products, { page: 1, limit: 2, id: 2 })
+  //   api
+  //     .get(endpoints.products, { page: 1, limit: 2, id: 2 })
+  //     .then(({ data }) => {
+  //       setData(data);
+  //       // console.log(data);
+  //     })
+  //     .catch((error) => {
+  //       // console.log(error);
+  //     });
+  // }, []);
+
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
-    // .get(endpoints.products, { page: 1, limit: 2, id: 2 })
-    api
-      .get(endpoints.products)
+    axios
+      .get('https://reqres.in/api/products', { params: { per_page: 5, page } })
       .then(({ data }) => {
         setData(data);
         // console.log(data);
@@ -19,7 +35,15 @@ const Root = () => {
       .catch((error) => {
         // console.log(error);
       });
-  }, []);
+  }, [page]);
+
+  const nextPage = () => {
+    setPage(page + 1);
+  };
+
+  const prevPage = () => {
+    setPage(page - 1);
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -38,6 +62,14 @@ const Root = () => {
           <h1>No available products</h1>
         )}
       </ProductsWrapper>
+      <PaginationWrapper>
+        <button onClick={prevPage}>
+          <strong>Previous Page</strong>
+        </button>
+        <button onClick={nextPage}>
+          <strong>Next Page</strong>
+        </button>
+      </PaginationWrapper>
     </ThemeProvider>
   );
 };
